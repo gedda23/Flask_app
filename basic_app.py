1d25d79eceff87e39184895de72f11b3cd503a10
Aditya <saiaditya.gedda@gmail.com>
2020-10-02 17:28:11 +0530

    Intial commit

---
+++ basic_app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+posts = [{
+    'Author' :'Venkata Sai Aditya Gedda',
+    'Title'  :'Blog Post1',
+    'Content':'First Post Content'
+},
+{
+    'Author' :'Ruthvik Ramaswamy',
+    'Title'  :'Blog Post2',
+    'Content':'Second Post Content'
+}]
+
+@app.route("/")
+def home():
+    return render_template('Hello.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('About.html',title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
